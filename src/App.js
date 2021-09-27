8f921608a25279939bf35f6ffa4517e2b7dfc049
DariaKryv <krivosheevadarya@gmail.com>
2021-09-27 12:16:37 +0200

    creat a new repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      \<h1>You will see today a Miracle !</h1>
+      <p>Well known App sins 2021</p>
+      <h2>GET STARTED NOW</h2>
     </div>
   );
 }
